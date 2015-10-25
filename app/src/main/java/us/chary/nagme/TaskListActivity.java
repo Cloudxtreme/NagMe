5cdb830bf98ce20dcfbe180e5046536b9c19a008
Zach Bloomquist <zrbq@live.com>
2015-10-25 18:29:28 -0400

    bugfixes for material design

--- app/src/main/java/us/chary/nagme/TaskListActivity.java
+++ app/src/main/java/us/chary/nagme/TaskListActivity.java
@@ -1,5 +1,6 @@
 package us.chary.nagme;
 
+import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
-public class TaskListActivity extends AppCompatActivity {
+public class TaskListActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +19,7 @@ public class TaskListActivity extends AppCompatActivity {
         TaskDBOpenHelper t = new TaskDBOpenHelper(this);
         SQLiteDatabase d = t.getReadableDatabase();
         String[] columns = new String[]{"rowid as _id","description", "due", "nags", "difficulty", "priority"};
-        Cursor c = d.query("tasks",columns,null,null,null,null,null);
+        Cursor c = d.query("tasks", columns, null, null, null, null, null);
         // fill listview with tasks
         ListView l = (ListView) findViewById(R.id.listView);
         l.setAdapter(new TaskListAdapter(this,c));
